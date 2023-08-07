548db3fb0a23464784006baea05e7eb69c5fd422
osamaline1229 <osama98138139@gmail.com>
2023-08-07 11:09:19 +0400

    customer and product details

---
+++ Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Customer
+{
+    public int CustomerId { get; set; }
+    public string FullName { get; set; }
+    public string PhoneNumber { get; set; }
+}
+
+class Product
+{
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+    public double UnitPrice { get; set; }
+}
